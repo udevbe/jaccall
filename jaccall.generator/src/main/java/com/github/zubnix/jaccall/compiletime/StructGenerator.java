a869e6788ead60ec3d13aa2d45756fbcaf152b6d
Erik De Rijcke <derijcke.erik@gmail.com>
2015-11-18 17:20:44 +0100

    don't return a null iterable of steps

--- jaccall.generator/src/main/java/com/github/zubnix/jaccall/compiletime/StructGenerator.java
+++ jaccall.generator/src/main/java/com/github/zubnix/jaccall/compiletime/StructGenerator.java
@@ -4,12 +4,13 @@ import com.google.auto.common.BasicAnnotationProcessor;
 import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.Processor;
+import java.util.Collections;
 
 @AutoService(Processor.class)
 public class StructGenerator extends BasicAnnotationProcessor {
 
     @Override
     protected Iterable<? extends ProcessingStep> initSteps() {
-        return null;
+        return Collections.emptyList();
     }
 }
