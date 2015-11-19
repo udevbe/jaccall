181471ab8f0e61527373acfb30d8c97644694427
Erik De Rijcke <derijcke.erik@gmail.com>
2015-11-19 08:44:03 +0100

    begin implementing struct runtime unit tests

---
+++ jaccall.runtime/src/test/java/com/github/zubnix/libtest/TestUnion.java
@@ -0,0 +1,15 @@
+package com.github.zubnix.libtest;
+
+import com.github.zubnix.jaccall.CType;
+import com.github.zubnix.jaccall.Field;
+import com.github.zubnix.jaccall.Struct;
+
+@Struct(union = true,
+        value = {
+                @Field(type = CType.SHORT,
+                       name = "field0"),
+                @Field(type = CType.FLOAT,
+                       name = "field1"),
+        })
+public class TestUnion extends TestUnion_Jaccall_StructType{
+}
---
+++ jaccall.runtime/src/test/java/com/github/zubnix/libtest/TestUnion_Jaccall_StructType.java
@@ -0,0 +1,34 @@
+package com.github.zubnix.libtest;
+
+import com.github.zubnix.jaccall.Size;
+import com.github.zubnix.jaccall.StructType;
+
+/**
+ * Created by zubzub on 11/19/15.
+ */
+public class TestUnion_Jaccall_StructType extends StructType {
+
+    public static final int SIZE = Size.sizeof((byte) 0) > Size.sizeof((float) 0) ? Size.sizeof((byte) 0) : Size.sizeof((float) 0);
+
+    TestUnion_Jaccall_StructType() {
+        super(SIZE);
+    }
+
+    public final byte field0() {
+        return buffer().get(0);
+    }
+
+    public final void field0(byte field0) {
+        buffer().put(0,
+                     field0);
+    }
+
+    public final float field1() {
+        return buffer().getFloat(0);
+    }
+
+    public final void field1(float field0) {
+        buffer().putFloat(0,
+                          field0);
+    }
+}
