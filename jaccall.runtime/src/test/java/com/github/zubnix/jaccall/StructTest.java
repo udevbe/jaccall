f34da57f153e7af70269337274d65868a6adca1d
Erik De Rijcke <derijcke.erik@gmail.com>
2015-11-25 05:34:05 +0100

    start preparing struct runtime unit tests

---
+++ jaccall.runtime/src/test/java/com/github/zubnix/jaccall/StructTest.java
@@ -0,0 +1,20 @@
+package com.github.zubnix.jaccall;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class StructTest {
+
+    @Test
+    public void testReadStructFieldTypes(){
+
+    }
+
+    @Test
+    public void testWriteStructFieldTypes(){
+
+    }
+}
