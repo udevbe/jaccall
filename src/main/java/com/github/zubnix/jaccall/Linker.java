cf60ea6bb7a64e1531fce8df523c0a4e61829265
Erik De Rijcke <derijcke.erik@gmail.com>
2015-12-10 16:15:27 +0100

    continue implementing linker lib

--- src/main/java/com/github/zubnix/jaccall/Linker.java
+++ src/main/java/com/github/zubnix/jaccall/Linker.java
@@ -1,6 +1,7 @@
 package com.github.zubnix.jaccall;
 
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
@@ -37,9 +38,97 @@ public final class Linker {
                 final String symbol = method.getName();
                 final long address = JNI.sym(handle,
                                              symbol);
+                final String signature = getFFISignature(method);
 
                 //TODO create method signatures for all methods and pass that to native side to setup libffi closures
             }
         }
+    }
+
+    //TODO this is something we can infer at compile time but how to get to it at runtime?
+    private static String getFFISignature(final Method method) {
+        final Class<?>     returnType            = method.getReturnType();
+        final Annotation[] returnTypeAnnotations = method.getAnnotations();
+        //TODO checking if ByVal annotation is done on a method that returns `long` should be done at compile time.
+
+        final Class<?>[]     parameterTypes       = method.getParameterTypes();
+        final Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+        //TODO checking if any ByVal annotation is done on a method parameter of type `long` should be done at compile time.
+
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            sb.append(typeToSignature(parameterTypes[i],
+                                      parameterAnnotations[i]));
+        }
+        sb.append(')')
+          .append(typeToSignature(returnType,
+                                  returnTypeAnnotations));
+
+
+        return sb.toString();
+    }
+
+    private static char typeToSignature(Class<?> type,
+                                        final Annotation[] parameterAnnotation) {
+//        'B'	bool
+//        'c'	char
+//        'C'	unsigned char
+//        's'	short
+//        'S'	unsigned short
+//        'i'	int
+//        'I'	unsigned int
+//        'j'	long
+//        'J'	unsigned long
+//        'l'	long long
+//        'L'	unsigned long long
+//        'f'	float
+//        'd'	double
+//        'p'	C pointer
+//        'Z'	char*
+//        'x'	SEXP
+//        'v'	void
+
+        if (type.equals(Integer.class) || type.equals(int.class)) {
+            return 'i';
+        }
+
+        if (type.equals(Float.class) || type.equals(float.class)) {
+            return 'f';
+        }
+
+        if (type.equals(Byte.class) || type.equals(byte.class)) {
+            return 'c';
+        }
+
+        if (type.equals(Short.class) ||
+            type.equals(short.class) ||
+            type.equals(Character.class) ||
+            type.equals(char.class)) {
+            return 's';
+        }
+
+        if (type.equals(Long.class) || type.equals(long.class)) {
+            for (Annotation annotation : parameterAnnotation) {
+                if(annotation instanceof Ptr){
+                    return 'p';
+                }
+                if(annotation instanceof ByVal){
+                    return 't';
+                }
+            }
+
+
+            return ' ';
+        }
+
+        if (type.equals(Double.class) || type.equals(double.class)) {
+            return 'd';
+        }
+
+        if (type.equals(CLong.class)) {
+            return 'l';
+        }
+
+        throw new IllegalArgumentException("Type " + type + " does not have a known native representation.");
     }
 }
