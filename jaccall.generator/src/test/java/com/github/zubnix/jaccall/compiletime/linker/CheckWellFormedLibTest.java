091a0a76305df6d3a7589174af092c2d468d6030
Erik De Rijcke <derijcke.erik@gmail.com>
2015-12-21 10:56:24 +0100

    Revert "implement generator test cases as compiled checked java source resources"
    This reverts commit edef85f71399319f2d6ed1b6b997a73b8e22c102.

--- jaccall.generator/src/test/java/com/github/zubnix/jaccall/compiletime/linker/CheckWellFormedLibTest.java
+++ jaccall.generator/src/test/java/com/github/zubnix/jaccall/compiletime/linker/CheckWellFormedLibTest.java
@@ -1,7 +1,6 @@
 package com.github.zubnix.jaccall.compiletime.linker;
 
 
-import com.github.zubnix.jaccall.compiletime.linker.testcasesource.LibNotOnClass;
 import com.google.testing.compile.JavaFileObjects;
 import org.junit.Test;
 
@@ -13,19 +12,7 @@ import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
 public class CheckWellFormedLibTest {
 
     @Test
-    public void testLibNotOnClass() {
-        JavaFileObject fileObject = TestCaseUtil.get(LibNotOnClass.class);
-        assert_().about(javaSource())
-                 .that(fileObject)
-                 .processedWith(new LinkerGenerator())
-                 .failsToCompile()
-                 .withErrorContaining("@Lib annotation should be placed on class type only.")
-                 .in(fileObject);
-    }
-
-    @Test
     public void testNotAPrimitive() {
-        //TODO move to it's own source file to catch compile time errors early
         JavaFileObject fileObject = JavaFileObjects.forSourceString("com.github.zubnix.libtest.Testing",
                                                                     "package com.github.zubnix.libtest;\n" +
                                                                     "import com.github.zubnix.jaccall.Lib;\n" +
@@ -45,12 +32,13 @@ public class CheckWellFormedLibTest {
                  .processedWith(new LinkerGenerator())
                  .failsToCompile()
                  .withErrorContaining("Native method should have supported primitive types only.")
-                 .in(fileObject);
+                 .in(fileObject)
+                 .onLine(8)
+                 .atColumn(31);
     }
 
     @Test
     public void testPtrAnnotationNotOnPrimitiveLong() {
-        //TODO move to it's own source file to catch compile time errors early
         JavaFileObject fileObject = JavaFileObjects.forSourceString("com.github.zubnix.libtest.Testing",
                                                                     "package com.github.zubnix.libtest;\n" +
                                                                     "import com.github.zubnix.jaccall.Lib;\n" +
@@ -70,35 +58,8 @@ public class CheckWellFormedLibTest {
                  .processedWith(new LinkerGenerator())
                  .failsToCompile()
                  .withErrorContaining("@Ptr annotation can only be placed on primitive type 'long'.")
-                 .in(fileObject);
+                 .in(fileObject)
+                 .onLine(11)
+                 .atColumn(64);
     }
-
-    @Test
-    public void testByValAnnotationNotOnPrimitiveLong() {
-        //TODO move to it's own source file to catch compile time errors early
-        JavaFileObject fileObject = JavaFileObjects.forSourceString("com.github.zubnix.libtest.Testing",
-                                                                    "package com.github.zubnix.libtest;\n" +
-                                                                    "import com.github.zubnix.jaccall.Lib;\n" +
-                                                                    "import com.github.zubnix.jaccall.ByVal;\n" +
-                                                                    "import com.github.zubnix.jaccall.StructType;\n" +
-                                                                    "import com.github.zubnix.jaccall.Ptr;\n" +
-                                                                    "import com.github.zubnix.jaccall.Unsigned;\n" +
-                                                                    "\n" +
-                                                                    "@Lib(\"testing\")\n" +
-                                                                    "public class Testing {\n" +
-                                                                    "    public static native long doStaticTest(long tst,\n" +
-                                                                    "                                           byte field0,\n" +
-                                                                    "                                           @Unsigned short field1,\n" +
-                                                                    "                                           @ByVal(StructType.class) int field2,\n" +
-                                                                    "                                           @Ptr(int.class) long field3);\n" +
-                                                                    "}");
-        assert_().about(javaSource())
-                 .that(fileObject)
-                 .processedWith(new LinkerGenerator())
-                 .failsToCompile()
-                 .withErrorContaining("@ByVal annotation can only be placed on primitive type 'long'.")
-                 .in(fileObject);
-    }
-
-
 }
