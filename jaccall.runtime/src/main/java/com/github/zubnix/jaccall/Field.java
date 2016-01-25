f2d2bae3252699d4d8fdffd420b72db63daad9e0
Erik De Rijcke <derijcke.erik@gmail.com>
2016-01-25 16:28:12 +0100

    add pointer depth member to field annotation, to enable proper pointer-to-pointer struct field generation.

--- jaccall.runtime/src/main/java/com/github/zubnix/jaccall/Field.java
+++ jaccall.runtime/src/main/java/com/github/zubnix/jaccall/Field.java
@@ -1,5 +1,6 @@
 package com.github.zubnix.jaccall;
 
+import javax.annotation.Nonnegative;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -16,24 +17,26 @@ public @interface Field {
     CType type();
 
     /**
-     * The number of times the type of this field repeats. The cardinality is used to define the length of a nested
-     * array of type: {@link #type()}. When the cardinality is greater than one, the field will be represented as a pointer.
-     * <p/>
-     * A cardinality of < 1 will result in a compile time error.
-     *
-     * @return the number of times the the type of this field repeats inside the struct. Defaults to 1.
-     */
-    int cardinality() default 1;
-
-    /**
      * Define an embedded struct or union.
      * <p/>
      * Will only be interpreted in conjunction with {@link #type()} set to
-     * {@link CType#STRUCT}.
+     * {@link CType#STRUCT} or {@link CType#POINTER}.
      *
      * @return A class annotated with {@link Struct}. If no annotation is found a compile time error is thrown.
      */
     Class<?> dataType() default Void.class;
+
+    /**
+     * Define the depth of pointer-to-pointer field types. A pointer that does not references a pointer (void*) has a
+     * depth of 0, a pointer that references a  pointer (void**) has a depth of 1, a pointer-to-pointer-to-pointer
+     * (void***) has a depth of 2 etc.
+     * <p/>
+     * Will only be interpreted in conjunction with {@link #type()} set to {@link CType#POINTER}.
+     *
+     * @return The pointer-to-pointer depth.
+     */
+    @Nonnegative
+    int pointerDepth() default 0;
 
     /**
      * The name of the field.
@@ -41,4 +44,15 @@ public @interface Field {
      * @return A name to identify a field. A name should be unique within a struct or type.
      */
     String name();
+
+    /**
+     * The number of times the type of this field repeats. The cardinality is used to define the length of a nested
+     * array of type: {@link #type()}. When the cardinality is greater than one, the field will be represented as a pointer.
+     * <p/>
+     * A cardinality of < 1 will result in a compile time error.
+     *
+     * @return the number of times the the type of this field repeats inside the struct. Defaults to 1.
+     */
+    @Nonnegative
+    int cardinality() default 1;
 }
