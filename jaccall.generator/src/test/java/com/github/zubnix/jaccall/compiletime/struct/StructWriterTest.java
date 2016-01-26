a432f232a3bf9b98a42b0c6aad3f57d92ff7607b
Erik De Rijcke <derijcke.erik@gmail.com>
2016-01-26 16:21:28 +0100

    implement struct double field generator unit test

--- jaccall.generator/src/test/java/com/github/zubnix/jaccall/compiletime/struct/StructWriterTest.java
+++ jaccall.generator/src/test/java/com/github/zubnix/jaccall/compiletime/struct/StructWriterTest.java
@@ -613,8 +613,56 @@ public class StructWriterTest {
 
     @Test
     public void testDoubleField() {
-        //TODO
-
+        //given
+        final JavaFileObject fileObject = JavaFileObjects.forSourceString("com.github.zubnix.libtest.struct.TestStructDouble",
+                                                                          "package com.github.zubnix.libtest.struct;\n" +
+                                                                          "\n" +
+                                                                          "import com.github.zubnix.jaccall.CType;\n" +
+                                                                          "import com.github.zubnix.jaccall.Field;\n" +
+                                                                          "import com.github.zubnix.jaccall.Struct;\n" +
+                                                                          "\n" +
+                                                                          "import static com.github.zubnix.jaccall.CType.DOUBLE;\n" +
+                                                                          "\n" +
+                                                                          "@Struct(value = {\n" +
+                                                                          "             @Field(type = DOUBLE,\n" +
+                                                                          "                    name = \"field0\")})\n" +
+                                                                          "public final class TestStructDouble extends TestStructDouble_Jaccall_StructType{\n" +
+                                                                          " \n" +
+                                                                          "}");
+        //when
+        final CompileTester compileTester = assert_().about(javaSource())
+                                                     .that(fileObject)
+                                                     .processedWith(new StructGenerator());
+        //then
+        compileTester.compilesWithoutError()
+                     .and()
+                     .generatesSources(JavaFileObjects.forSourceString("com.github.zubnix.libtest.struct.TestStructDouble_Jaccall_StructType",
+                                                                       "package com.github.zubnix.libtest.struct;\n" +
+                                                                       "\n" +
+                                                                       "import com.github.zubnix.jaccall.JNI;\n" +
+                                                                       "import com.github.zubnix.jaccall.StructType;\n" +
+                                                                       "import javax.annotation.Generated;\n" +
+                                                                       "\n" +
+                                                                       "@Generated(\"com.github.zubnix.jaccall.compiletime.struct.StructGenerator\")\n" +
+                                                                       "abstract class TestStructDouble_Jaccall_StructType extends StructType {\n" +
+                                                                       "  public static final long FFI_TYPE = JNI.ffi_type_struct(JNI.FFI_TYPE_DOUBLE);\n" +
+                                                                       "\n" +
+                                                                       "  public static final int SIZE = JNI.ffi_type_struct_size(FFI_TYPE);\n" +
+                                                                       "\n" +
+                                                                       "  private static final int OFFSET_0 = 0;\n" +
+                                                                       "\n" +
+                                                                       "  TestStructDouble_Jaccall_StructType() {\n" +
+                                                                       "    super(SIZE);\n" +
+                                                                       "  }\n" +
+                                                                       "\n" +
+                                                                       "  public final double field0() {\n" +
+                                                                       "    return readDouble(OFFSET_0);\n" +
+                                                                       "  }\n" +
+                                                                       "\n" +
+                                                                       "  public final void field0(final double field0) {\n" +
+                                                                       "    writeDouble(OFFSET_0, field0);\n" +
+                                                                       "  }\n" +
+                                                                       "}"));
     }
 
     @Test
