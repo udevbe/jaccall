448b7a998776f477e0fa3ac335ec868eda3dc1e0
Erik De Rijcke <derijcke.erik@gmail.com>
2016-01-26 15:55:58 +0100

    implement struct long long field generator unit test

--- jaccall.generator/src/test/java/com/github/zubnix/jaccall/compiletime/struct/StructWriterTest.java
+++ jaccall.generator/src/test/java/com/github/zubnix/jaccall/compiletime/struct/StructWriterTest.java
@@ -451,8 +451,56 @@ public class StructWriterTest {
 
     @Test
     public void testLongLongField() {
-        //TODO
-
+        //given
+        final JavaFileObject fileObject = JavaFileObjects.forSourceString("com.github.zubnix.libtest.struct.TestStructLongLong",
+                                                                          "package com.github.zubnix.libtest.struct;\n" +
+                                                                          "\n" +
+                                                                          "import com.github.zubnix.jaccall.CType;\n" +
+                                                                          "import com.github.zubnix.jaccall.Field;\n" +
+                                                                          "import com.github.zubnix.jaccall.Struct;\n" +
+                                                                          "\n" +
+                                                                          "import static com.github.zubnix.jaccall.CType.LONG_LONG;\n" +
+                                                                          "\n" +
+                                                                          "@Struct(value = {\n" +
+                                                                          "             @Field(type = LONG_LONG,\n" +
+                                                                          "                    name = \"field0\")})\n" +
+                                                                          "public final class TestStructLongLong extends TestStructLongLong_Jaccall_StructType{\n" +
+                                                                          " \n" +
+                                                                          "}");
+        //when
+        final CompileTester compileTester = assert_().about(javaSource())
+                                                     .that(fileObject)
+                                                     .processedWith(new StructGenerator());
+        //then
+        compileTester.compilesWithoutError()
+                     .and()
+                     .generatesSources(JavaFileObjects.forSourceString("com.github.zubnix.libtest.struct.TestStructLongLong_Jaccall_StructType",
+                                                                       "package com.github.zubnix.libtest.struct;\n" +
+                                                                       "\n" +
+                                                                       "import com.github.zubnix.jaccall.JNI;\n" +
+                                                                       "import com.github.zubnix.jaccall.StructType;\n" +
+                                                                       "import javax.annotation.Generated;\n" +
+                                                                       "\n" +
+                                                                       "@Generated(\"com.github.zubnix.jaccall.compiletime.struct.StructGenerator\")\n" +
+                                                                       "abstract class TestStructLongLong_Jaccall_StructType extends StructType {\n" +
+                                                                       "  public static final long FFI_TYPE = JNI.ffi_type_struct(JNI.FFI_TYPE_SINT64);\n" +
+                                                                       "\n" +
+                                                                       "  public static final int SIZE = JNI.ffi_type_struct_size(FFI_TYPE);\n" +
+                                                                       "\n" +
+                                                                       "  private static final int OFFSET_0 = 0;\n" +
+                                                                       "\n" +
+                                                                       "  TestStructLongLong_Jaccall_StructType() {\n" +
+                                                                       "    super(SIZE);\n" +
+                                                                       "  }\n" +
+                                                                       "\n" +
+                                                                       "  public final long field0() {\n" +
+                                                                       "    return readLong(OFFSET_0);\n" +
+                                                                       "  }\n" +
+                                                                       "\n" +
+                                                                       "  public final void field0(final long field0) {\n" +
+                                                                       "    writeLong(OFFSET_0, field0);\n" +
+                                                                       "  }\n" +
+                                                                       "}"));
     }
 
     @Test
---
+++ jaccall.generator/src/test/java/com/github/zubnix/libtest/struct/TestStructLongLong_Jaccall_StructType.java
@@ -0,0 +1,10 @@
+package com.github.zubnix.libtest.struct;
+
+import com.github.zubnix.jaccall.StructType;
+
+abstract class TestStructLongLong_Jaccall_StructType extends StructType {
+
+    TestStructLongLong_Jaccall_StructType() {
+        super(0);
+    }
+}
