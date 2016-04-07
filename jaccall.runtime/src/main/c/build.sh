e26a1522a12d07fbddc6905941830b86958ffbe0
Erik De Rijcke <derijcke.erik@gmail.com>
2016-04-07 09:52:32 +0200

    add arch separated build structure

--- jaccall.runtime/src/main/c/build.sh
+++ jaccall.runtime/src/main/c/build.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 set -e;
 
-ARCH="$(uname -m)";
-BUILD_DIR="build/$ARCH";
+#TODO add android
+ARCHS=("linux-aarch64" "linux-armv7hf" "linux-armv7sf" "linux-armv6hf" "linux-x86_64" "linux-i686");
 
 prep_build() {
+    ARCH=$1
+    BUILD_DIR="build/${ARCH}";
+
     rm -rf $BUILD_DIR;
     mkdir -p $BUILD_DIR;
 }
 
 build() {
+    ARCH=$1
+    BUILD_DIR="build/${ARCH}";
+
     pushd $BUILD_DIR;
+    #TODO use docker to cross compile library based on arch & os
     cmake ../..
     make;
     popd;
 }
 
-pushd jaccall;
-prep_build;
-build;
-popd;
+main() {
+    pushd jaccall;
+
+    for ARCH in "${ARCHS[@]}"
+    do
+        prep_build "${ARCH}";
+        build "${ARCH}";
+    done
+
+    popd;
+}
+
+main
